ba26aeb374439ee58c2d51d39b5740d014f68fd0
Sandra Julingit config --global user.name Sandra <sj223zn@student.lnu.se>
2024-10-23 20:34:48 +0200

    Try to fix deploying issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@items': '/src',
-      '@themes': '/src',
-      '@computers': '/src/',
-      '@computerrows': '/src/',
-      '@games': '/src',
-      '@index': '/src/',
-      '@gameuis': '/src/',
-      '@gameboards': '/src/'
-    },
   },
 });
